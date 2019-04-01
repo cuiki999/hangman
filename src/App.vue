acb0cbff25158c08add70ecd3219d5cd9a026a11
Lea <lea.opacic@gmail.com>
2019-04-01 22:43:22 +0200

    Add user interface

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <h1>HANGMAN</h1>
+    <span class="streak">Streak: {{ streak }}&nbsp;&nbsp;|&nbsp;&nbsp;Best: {{ best }}</span>
+    <img class="cog" title="Settings" src="./assets/img/cog.png" />
     <div id="paper">
-      <img class="paper" src="./assets/img/big-paper.png" />
+      <img class="paper" src="./assets/img/grey-tape.png" />
       <div id="hangman-pics">
         <!-- :src is short for v-bind:src -->
         <img :src="require(`./assets/img/${imgNumber}.png`)" />
@@ -11,23 +13,26 @@
         <span v-html="compLetter"></span>
       </div>
     </div>
-    <br><br>
 
     <div id="user-console">
-      <div class="user-letters" v-for="userLetter in userLetters.slice(0,9)" @click="!userLetter.grey ? guessLetter(userLetter.letter) : ''; userLetter.grey = true">
-        <span v-bind:class="{ inactive: userLetter.grey }">{{ userLetter.letter }}</span>
-        <div class="hexagon"></div>
+      <div class="user-letters" v-for="userLetter in userLetters.slice(0,9)" @click="userLetter.hex === 1 ? guessLetter(userLetter.letter) : ''">
+        <span>{{ userLetter.letter }}</span>
+        <img class="hexagon" :src="require(`./assets/img/hex${userLetter.hex}.png`)" />
       </div>
       <br>
-      <div class="user-letters" v-for="userLetter in userLetters.slice(9,17)" @click="!userLetter.grey ? guessLetter(userLetter.letter) : ''; userLetter.grey = true">
-        <span v-bind:class="{ inactive: userLetter.grey }">{{ userLetter.letter }}</span>
+      <div class="user-letters" v-for="userLetter in userLetters.slice(9,17)" @click="userLetter.hex === 1 ? guessLetter(userLetter.letter) : ''">
+        <span>{{ userLetter.letter }}</span>
+        <img class="hexagon" :src="require(`./assets/img/hex${userLetter.hex}.png`)" />
       </div>
       <br>
-      <div class="user-letters" v-for="userLetter in userLetters.slice(17,26)" @click="!userLetter.grey ? guessLetter(userLetter.letter) : ''; userLetter.grey = true">
-        <span v-bind:class="{ inactive: userLetter.grey }">{{ userLetter.letter }}</span>
+      <div class="user-letters" v-for="userLetter in userLetters.slice(17,26)" @click="userLetter.hex === 1 ? guessLetter(userLetter.letter) : ''">
+        <span>{{ userLetter.letter }}</span>
+        <img class="hexagon" :src="require(`./assets/img/hex${userLetter.hex}.png`)" />
       </div>
     </div>
 
+
+    <br><br><br><br><br><br><br><br><br><br>
     <div>
       <p>Number of failed attempts: 
         <span class="setting" v-bind:class="{ selected: !difficult }" @click="difficult = false; reset('Resetting...')">10</span> / 
@@ -55,9 +60,12 @@ export default {
       word: '',
       userLetters: [],
       compLetters: [],
-      allLeters: [],
+      allLetters: [],
       difficult: false,
-      startingLettersOn: false
+      startingLettersOn: false,
+      streak: 0,
+      best: 0,
+      resetting: false
     }
   },
   components: {
@@ -67,9 +75,9 @@ export default {
     comeUpWithWord() {
       let random = Math.floor(Math.random() * Wordlist.nouns.length);
       this.word = Wordlist.nouns[random];
-      if (this.word.length < 13) {
+      if (this.word.length < 15) {
         for (let i = 0; i < this.word.length; i++) {
-          this.compLetters.push("&nbsp;");        
+          this.compLetters.push("&nbsp;");     
           if (!/[a-zA-Z]/.test(this.word[i])) {
             this.compLetters[i] = this.word[i];
           }
@@ -78,7 +86,7 @@ export default {
           let randomLetter = Math.floor(Math.random() * this.word.length);
           let letter = this.word.charAt(randomLetter).toUpperCase();
           this.guessLetter(letter);
-          this.userLetters[this.allLeters.indexOf(letter)].grey = true;
+          this.userLetters[this.allLetters.indexOf(letter)].grey = true;
         }
       } else {
         this.comeUpWithWord();
@@ -86,23 +94,28 @@ export default {
       
     },
     guessLetter(letter) {
+      // see which index a letter is at so its hexagon could be turned orange or grey
+      let userIndex = this.allLetters.indexOf(letter);
       if (this.word.toUpperCase().indexOf(letter) !== -1) {
         // an array of all the indexes of a certain letter, because the letter can appear more than once within a word
         let indexes = [];
+        this.userLetters[userIndex].hex = 2; 
         for (let i = 0; i < this.word.length; i++) {
           if (this.word.toUpperCase()[i] === letter) {
             indexes.push(i);
             for (let j = 0; j < indexes.length; j++) {
               let number = indexes[j];
-              this.compLetters[number] = letter;
+              this.compLetters[number] = letter;  
               this.checkIfVictory();
             }
           }
         }
       } else {
+        this.userLetters[userIndex].hex = 3;
         if (this.imgNumber === 5 && this.difficult === true) {
           this.imgNumber = 11;
-          this.reset('You lose');            
+          this.reset('You lose');
+          this.userLetters.hex = 2;            
         }
         else if (this.imgNumber < 10) {
           this.imgNumber++;
@@ -116,9 +129,8 @@ export default {
       // generate the userLetters array by looping through ASCII codes: 65 = A etc.
       for (let i = 65; i < 91; i++) {
         let letter = String.fromCharCode(i);
-        this.userLetters.push({'letter': letter, 'grey': false});
-        // make this array of letters so you can refer to it when making a letter turn gray in comeUpWithWord() for when startingLettersOn is set to true
-        this.allLeters.push(letter);
+        this.userLetters.push({'letter': letter, 'hex': 1});
+        this.allLetters.push(letter);
       }
     },
     checkIfVictory() {
@@ -127,24 +139,51 @@ export default {
       }
     },
     reset(message) {
+      this.resetting = true;
       for (let i = 0; i < this.word.length; i++) {
         if (this.compLetters[i] === '&nbsp;') {
           this.compLetters[i] = this.word[i].toUpperCase();
         }        
       }
+      if (message === 'You won!') {
+        this.streak += 1;
+        if (this.streak > this.best) {
+          this.best = this.streak;
+          localStorage.setItem('best', this.streak);
+        }
+      } else if (message === 'You lose') {
+        this.streak = 0;
+      }
       setTimeout(() => {
-        alert(message);
+        this.resetting = false;
         this.imgNumber = 0;
         this.userLetters = [];
         this.compLetters = [];
         this.generateUserArray();
         this.comeUpWithWord();
+        alert(message);
       }, 1000)
+    },
+    onkey(event) {
+      if (event.keyCode > 64 && event.keyCode < 91 && this.resetting === false) {
+        let letter = String.fromCharCode(event.keyCode);
+        let userIndex = this.allLetters.indexOf(letter);
+        if (this.userLetters[userIndex].hex === 1) {  
+          this.guessLetter(letter);
+        }
+      }
     }
   },
   created() {
-  this.generateUserArray();
-  this.comeUpWithWord();    
+    this.generateUserArray();
+    this.comeUpWithWord();
+    localStorage.getItem('best') ? this.best = localStorage.getItem('best') : '';
+  },
+  mounted() {
+    window.addEventListener('keyup', this.onkey);
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('keyup', this.onkey)
   }
 }
 </script>
@@ -153,7 +192,7 @@ export default {
 
 #app {
   background-color: #CDDEEB;
-  width: 1000px;
+  width: 700px;
   height: 1000px;
   margin: 0 auto;
 }
@@ -164,14 +203,30 @@ h1 {
   font-size: 50px;
 }
 
+.streak {
+  font-family: 'Crimson Text', serif;
+  font-size: 1.5rem;
+  position: absolute;
+  top: 10px;
+  right: 15px;
+}
+
+.cog {
+  position: absolute;
+  max-width: 50px;
+  cursor: pointer;
+  top: 60px;
+  right: 15px;
+}
+
 #paper {
   text-align: center;
   margin-top: -20px;
   //border: 1px solid red;
-  margin-bottom: -200px;
+  margin-bottom: -120px;
 
   .paper {
-    width: 60%;
+    width: 90%;
   }
 }
 
@@ -201,17 +256,30 @@ h1 {
   display: block;
   text-align: center;
   margin-top: 30px;
-}
 
-.user-letters {
+  .user-letters {
   font-family: 'Crimson Text', serif;
+  color: #fff;
   font-size: 2rem;
-  min-width: 3rem;
+  width: 4.5rem;
   cursor: pointer;
   font-weight: bold;
   display: inline-block;
-  margin-right: 1rem;
-  margin-bottom: 1rem;
+  margin-top: -26px;
+
+    span {
+      z-index: 100;
+      position: relative;
+      min-width: 3rem;
+      text-align: center;
+      top: 18px;
+    }
+
+    .hexagon {
+      position: relative;
+      margin-top: -80px;
+    }
+  }
 }
 
 .inactive {
@@ -226,42 +294,5 @@ h1 {
   font-weight: bold;
   color: orange;
 }
-
-.hexagon {
-  position: relative;
-  width: 40px; 
-  height: 23.09px;
-  background-color: #7eaacb;
-  margin: 11.55px 0;
-  border-left: 4px solid #ffffff;
-  border-right: 4px solid #ffffff;
-}
-
-.hexagon:before,
-.hexagon:after {
-  content: "";
-  position: absolute;
-  z-index: 1;
-  width: 28.28px;
-  height: 28.28px;
-  -webkit-transform: scaleY(0.5774) rotate(-45deg);
-  -ms-transform: scaleY(0.5774) rotate(-45deg);
-  transform: scaleY(0.5774) rotate(-45deg);
-  background-color: inherit;
-  left: 2.8579px;
-}
-
-.hexagon:before {
-  top: -14.1421px;
-  border-top: 4px solid #ffffff;
-  border-right: 4px solid #ffffff;
-}
-
-.hexagon:after {
-  bottom: -14.1421px;
-  border-bottom: 4px solid #ffffff;
-  border-left: 4px solid #ffffff;
-}
-
 
 </style>
