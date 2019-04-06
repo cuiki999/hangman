3514dc77b84642f615efdc6a957a78964155b0e4
Lea <lea.opacic@gmail.com>
2019-04-06 21:49:21 +0200

    Add settings

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>HANGMAN</h1>
     <span class="streak">Streak: {{ streak }}&nbsp;&nbsp;|&nbsp;&nbsp;Best: {{ best }}</span>
-    <div class= "cog-container" title="Settings"></div>
+    <div class= "cog-container" title="Settings" @click="$refs.settings.style.display = 'block'"></div>
     <div id="paper">      
       <img class="paper-img" src="./assets/img/grey-tape.png" />
       <div id="hangman-pics">
@@ -34,9 +34,43 @@
       </div> 
     </div>
 
-    <br><br><br><br><br><br><br><br><br><br>
-    <div>
-      <p>Number of failed attempts: 
+    <div id="settings" ref="settings">
+      <div id="settings-all">
+        <div class="close" @click="updateHexes('close')">
+          <span>X</span>
+        </div>
+        <div class="options">
+          <p>Number of failed attempts:</p>
+          <div v-on:click="changeDiffHex(0)">
+            <span>10</span>
+            <img class="option-hex" :src="require(`./assets/img/hex${settings.tdHex[0]}.png`)" />  
+          </div>
+          <div v-on:click="changeDiffHex(1)">
+            <span>5</span>
+            <img class="option-hex" :src="require(`./assets/img/hex${settings.tdHex[1]}.png`)" />
+          </div>
+        </div>
+        <div class="options">
+          <p>Start with one letter displayed:</p>
+          <div @click="changeStartHex(0)">
+            <span>Y</span>
+            <img class="option-hex" :src="require(`./assets/img/hex${settings.tsHex[0]}.png`)" />
+          </div>
+          <div @click="changeStartHex(1)">  
+            <span>N</span>
+            <img class="option-hex" :src="require(`./assets/img/hex${settings.tsHex[1]}.png`)" />
+          </div>
+        </div>
+        <br><br>
+        <span class="warning">Applying the changes will end the current game.</span>
+        <br><br>
+        <button v-show="!settings.changedDiff && !settings.changedStart">APPLY CHANGES</button>
+        <button class="activebutton" v-show="settings.changedDiff || settings.changedStart" @click="applyChanges(); updateHexes('apply')">APPLY CHANGES</button>
+      </div>
+    </div>
+
+    <!--
+    <p>Number of failed attempts: 
         <span class="setting" v-bind:class="{ selected: !difficult }" @click="difficult = false; reset('Resetting...')">10</span> / 
         <span class="setting" v-bind:class="{ selected: difficult }" @click="difficult = true; reset('Resetting...')">5</span>
       </p>
@@ -44,7 +78,8 @@
         <span class="setting" v-bind:class="{ selected: startingLettersOn }" @click="startingLettersOn = true; reset('Resetting...')">yes</span> / 
         <span class="setting" v-bind:class="{ selected: !startingLettersOn }" @click="startingLettersOn = false; reset('Resetting...')">no</span>
       </p>
-    </div>
+    -->
+
   </div>
 </template>
 
@@ -58,23 +93,32 @@ export default {
   },
   data () {
     return {
+      word: '',
+      compLetters: [],
+      userLetters: [],
+      allLetters: [],
+      imgNumber: 0,
       streak: 0,
       best: 0,
-      difficult: false,
-      startingLettersOn: false,
-      imgNumber: 0,
-      word: '',
-      userLetters: [],
-      compLetters: [],
-      allLetters: [],
       resetting: false,
-      gameEnd: true,
       displayMessage: false,
       gameEndMessage: '',
+      settings: {
+        // (temporary) difficulty hex & (temporary) starting-letter-on hex
+        // temporary is what is visible in the settings menu, non-temporary is what actually gets saved
+        // if you press 'apply', temporary becomes non-temporary. If you press X, it's the other way around
+        // 2 means selected (orange), 3 means not selected (gray)
+        dHex: [2, 3],
+        tdHex: [2, 3],
+        sHex: [3, 2],
+        tsHex: [3, 2],
+        changedDiff: false,
+        changedStart: false,
+        difficult: false,
+        startingLettersOn: false,
+        madeChanges: false,
+      }
     }
-  },
-  components: {
-
   },
   methods: {
     comeUpWithWord() {
@@ -87,11 +131,10 @@ export default {
             this.compLetters[i] = this.word[i];
           }
         }
-        if (this.startingLettersOn) {
+        if (this.settings.startingLettersOn) {
           let randomLetter = Math.floor(Math.random() * this.word.length);
           let letter = this.word.charAt(randomLetter).toUpperCase();
           this.guessLetter(letter);
-          this.userLetters[this.allLetters.indexOf(letter)].grey = true;
         }
       } else {
         this.comeUpWithWord();
@@ -116,7 +159,7 @@ export default {
         }
       } else {
         this.userLetters[userIndex].hex = 3;
-        if (this.imgNumber === 5 && this.difficult === true) {
+        if (this.imgNumber === 5 && this.settings.difficult === true) {
           this.imgNumber = 11;
           this.reset('YOU LOST');
           this.userLetters.hex = 2;            
@@ -168,6 +211,7 @@ export default {
         this.compLetters = [];
         this.generateUserArray();
         this.comeUpWithWord();
+        
       }, 2000);
     },
     onkey(event) {
@@ -178,6 +222,42 @@ export default {
           this.guessLetter(letter);
         }
       }
+    },
+    changeDiffHex(hex) {
+      hex === 0 ? this.settings.tdHex = [2, 3] : this.settings.tdHex = [3, 2];
+      this.settings.tdHex[0] == this.settings.dHex[0] ? this.settings.changedDiff = false : this.settings.changedDiff = true;
+    },
+    changeStartHex(hex) {
+      hex === 0 ? this.settings.tsHex = [2, 3] : this.settings.tsHex = [3, 2];
+      this.settings.tsHex[0] == this.settings.sHex[0] ? this.settings.changedStart = false : this.settings.changedStart = true;
+    },
+    updateHexes(option) {
+      this.$refs.settings.style.display = 'none'; 
+      this.settings.changedDiff = false;
+      this.settings.changedStart = false;
+      if (option === 'close') {
+        this.settings.tdHex = this.settings.dHex;
+        this.settings.tsHex = this.settings.sHex; 
+      } else if (option === 'apply') {
+        this.settings.dHex = this.settings.tdHex;
+        this.settings.sHex = this.settings.tsHex;
+      }      
+    },
+    applyChanges() {
+      let hexArray = [this.settings.tdHex, this.settings.tsHex];
+      // Number of failed attempts -> 10 is selected
+      if (hexArray[0][0] === 2) {
+        this.settings.difficult = false;
+      } else if (hexArray[0][0] === 3) {
+        this.settings.difficult = true;
+      }
+      // Start with one letter displayed -> Y is selected
+      if (hexArray[1][0] === 2) {
+        this.settings.startingLettersOn = true;
+      } else if (hexArray[1][0] === 3) {
+        this.settings.startingLettersOn = false;
+      }
+      this.reset('RESETTING...');
     }
   },
   created() {
@@ -202,16 +282,15 @@ export default {
   width: 700px;
   height: 1000px;
   margin: 0 auto;
+  font-family: 'Crimson Text', serif;
 }
 
 h1 {
-  font-family: 'Crimson Text', serif;
   text-align: center;
   font-size: 50px;
 }
 
 .streak {
-  font-family: 'Crimson Text', serif;
   font-size: 1.5rem;
   position: absolute;
   top: 10px;
@@ -300,11 +379,10 @@ h1 {
 
   .user-letters {
     display: inline-block;
-    font-family: 'Crimson Text', serif;
     font-size: 2rem;
     font-weight: bold;
     color: #fff;
-    margin-top: -26px;
+    margin-top: -24px;
     width: 4.5rem;
     cursor: pointer;
 
@@ -350,12 +428,10 @@ h1 {
     box-sizing: border-box;
   
     span {
-      font-family: 'Crimson Text', serif;
       font-size: 1.5rem;
     }
 
     button {
-      font-family: 'Crimson Text', serif;
       margin-top: 30px;
       background-color: #686968;
       color: #fff;
@@ -366,10 +442,105 @@ h1 {
   }
 }
 
+#settings {
+  display: none;
+  position: absolute;
+  width: 400px;
+  height: 430px;
+  top: 60px;
+  right: 15px;
+  border: 4px solid #fff;
+  box-shadow: 0.31rem 0.31rem 0.31rem 0 rgba(170, 170, 170, 0.8);
+  color: #fff;
+  background-color: #686968;
+  z-index: 500;
+  text-align: center;
+  -webkit-animation: grow 0.3s ease-in;
+  -moz-animation: grow 0.3s ease-in;
+  -o-animation: grow 0.3s ease-in;
+  animation: grow 0.3s ease-in;
+
+  #settings-all {
+    -webkit-animation: appear 0.5s linear;
+    -moz-animation: appear 0.5s linear;
+    -o-animation: appear 0.5s linear;
+    animation: appear 0.4s linear;  
+  }
+
+  .close {
+    font-family: 'Neucha', cursive;
+    position: relative;
+    left: 360px;
+    top: 13px;
+    width: 30px;
+    font-weight: bold;
+    font-size: 2rem;
+    cursor: pointer;
+    margin-bottom: 40px;
+  }
+
+  .options {
+    text-align: center;
+    margin-top: 30px;
+    display: block;
+    margin-bottom: -50px;
+    font-size: 1.3rem;
+
+    div {
+      display: inline-block;
+      text-align: center;
+      //border: 1px solid red;
+      width: 40px;
+      cursor: pointer;
+      height: 35px;
+      margin: 0 5px 0 5px;
+
+      .option-hex {
+        width: 40px;
+        //display: inline-block;
+        position: relative;
+        top: -37px;
 
 
-.inactive {
-  color: grey;
+        //margin-right: 20px;
+      }
+
+      span {
+        //display: inline-block;
+        //text-align: center;
+        position: relative;
+        //margin-right: -30px;
+        z-index: 200;
+
+      }
+
+    }
+
+    
+  }
+
+  .warning {
+    font-family: 'Neucha', cursive;
+    //font-size: 1rem;
+  }
+
+  button {
+    background-color: #686968;
+    margin-top: -10px;
+    border: 4px solid #fff;
+    pointer-events: none;
+    padding: 10px;
+    font-weight: bold;
+    font-family: 'Crimson Text', serif;
+    font-size: 1.2rem;
+    color: #fff;
+  }
+
+  .activebutton {
+    background-color: #DBC147;
+    cursor: pointer;
+    pointer-events: auto;
+  }
 }
 
 .setting {
@@ -385,13 +556,42 @@ h1 {
   0% {
     background-image: url("./assets/img/cog.png");
   }
+  49% {
+    background-image: url("./assets/img/cog.png");
+  }
   50% {
+    background-image: url("./assets/img/cog-turn.png");
+  }
+  99% {
     background-image: url("./assets/img/cog-turn.png");
   }
 }
 
 @keyframes fade-in {
   0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+@keyframes grow {
+  0% {
+    width: 0%;
+    height: 0%;
+  }
+  100% {
+    width: 400px;
+    height: 430px;
+  }
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  75% {
     opacity: 0;
   }
   100% {
